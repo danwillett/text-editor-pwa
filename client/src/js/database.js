c9d5dd08af3605bfdcf5e55fad7a3483b1c971d2
danwill <daniel.willett2@gmail.com>
2023-03-20 10:38:08 -0700

    added putDb route in database.js for indexDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    console.log('post to database')
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+  const request = store.add({ jate: content });
+  const result = await request; // we make this asynchronous so the user still has full functionality while things are getting posted to the DB
+  console.log('🚀 - data saved to the database', result);
+  } catch {
+    console.error('putDb not implemented')
+  }
+ 
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
